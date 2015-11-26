8e1eca3896e540b1118fb9055ec2a07bae3cfc00
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-11-26 10:58:33 +0700

    Add player character motions

--- src/entity/PlayerCharacter.java
+++ src/entity/PlayerCharacter.java
@@ -8,6 +8,8 @@ import render.Renderable;
 
 public class PlayerCharacter implements Renderable {
 
+	public static final int LEFT = -1, RIGHT = 1;
+	private int speed;
 	private int x, y;
 	
 	public PlayerCharacter() {
@@ -20,6 +22,16 @@ public class PlayerCharacter implements Renderable {
 	
 	public int getY() {
 		return y;
+	}
+
+	// Motion
+	public void walk(int direction) {
+		x += speed*direction;
+		// TODO play walking animation
+	}
+	
+	public void jump() {
+		// TODO implement jump
 	}
 
 	@Override
