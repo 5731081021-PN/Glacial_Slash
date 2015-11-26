45fa3f37dbdd643da66eb2a92d5b3e3fd926e65f
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-11-26 10:51:42 +0700

    Make InputUtility methods static

--- src/input/InputUtility.java
+++ src/input/InputUtility.java
@@ -6,27 +6,27 @@ package input;
 
 public class InputUtility {
 
-	private boolean[] keyPressed = new boolean[256], keyTriggered = new boolean[256];
+	private static boolean[] keyPressed = new boolean[256], keyTriggered = new boolean[256];
 	
-	public boolean getKeyPressed(int key) {
+	public static boolean getKeyPressed(int key) {
 		if (key < 0 || key >= keyPressed.length)
 			return false;
 		return keyPressed[key];
 	}
 	
-	public boolean getKeyTriggered(int key) {
+	public static boolean getKeyTriggered(int key) {
 		if (key < 0 || key >= keyTriggered.length)
 			return false;
 		return keyTriggered[key];
 	}
 	
-	public void setKeyPressed(int key, boolean pressed) {
+	public static void setKeyPressed(int key, boolean pressed) {
 		if (key < 0 || key >= keyPressed.length)
 			return;
 		keyPressed[key] = pressed;
 	}
 	
-	public void setKeyTriggered(int key, boolean triggered) {
+	public static void setKeyTriggered(int key, boolean triggered) {
 		if (key < 0 || key >= keyTriggered.length)
 			return;
 		keyTriggered[key] = triggered;
