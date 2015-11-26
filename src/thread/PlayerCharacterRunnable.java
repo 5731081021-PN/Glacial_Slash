8ff86b3ad305e5e9a31167200c88bd3dce05f2ff
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-11-26 10:52:07 +0700

    Add player field to PlayerCharacterRunnable

--- src/thread/PlayerCharacterRunnable.java
+++ src/thread/PlayerCharacterRunnable.java
@@ -4,7 +4,18 @@
 
 package thread;
 
+import java.awt.event.KeyEvent;
+
+import entity.PlayerCharacter;
+import input.InputUtility;
+
 public class PlayerCharacterRunnable implements Runnable {
+
+	private PlayerCharacter player;
+	
+	public PlayerCharacterRunnable(PlayerCharacter player) {
+		this.player = player;
+	}
 
 	public void run() {
 		// TODO Implement running
