2898051765a44c413d5c6097281f0f24b9c35f46
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-12-08 23:48:35 +0700

    Make SkillCard Comparable for hand sorting

--- src/entity/card/SkillCard.java
+++ src/entity/card/SkillCard.java
@@ -6,11 +6,20 @@ package entity.card;
 
 import render.Renderable;
 
-public abstract class SkillCard implements Renderable {
+public abstract class SkillCard implements Renderable, Comparable<SkillCard> {
 
 	protected int cost;
 	protected int[] command;
 	
 	public abstract void activate();
+	
+	@Override
+	public int compareTo(SkillCard other) {
+		int costCompare = Integer.compare(this.cost, other.cost);
+		if (costCompare == 0)
+			return this.getClass().getSimpleName().compareToIgnoreCase(other.getClass().getSimpleName());
+		else
+			return costCompare;
+	}
 
 }
