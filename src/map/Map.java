69055732796ea9fe7effd34d71d5add4bea6b771
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-12-08 14:44:21 +0700

    Map and Terrain foundation

---
+++ src/map/Map.java
@@ -0,0 +1,20 @@
+/*
+ * Represent each map of the game
+ */
+
+package map;
+
+import java.io.File;
+
+public class Map {
+
+	public Map(File mapFile) {
+		
+	}
+	
+	public int getTerrain(int x, int y) {
+		//TODO terrain
+		return 0;
+	}
+
+}
