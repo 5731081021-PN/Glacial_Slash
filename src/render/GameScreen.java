b2fd0a8e92c8705600af9ba71f3de88c432ee678
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-12-08 16:03:54 +0700

    Make GameScreen singleton

--- src/render/GameScreen.java
+++ src/render/GameScreen.java
@@ -5,5 +5,11 @@ import javax.swing.JComponent;
 public class GameScreen extends JComponent {
 
 	private static final long serialVersionUID = 8861317653703713044L;
+	
+	private static GameScreen screen = new GameScreen();
+	
+	public static GameScreen getScreen() {
+		return screen;
+	}
 
 }
