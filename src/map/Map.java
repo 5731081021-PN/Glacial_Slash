e18b0dac4ae6e58c6e0c81060eb9039ccbac146e
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-12-08 16:03:43 +0700

    Mark offmap as Rigid bodies

--- src/map/Map.java
+++ src/map/Map.java
@@ -6,14 +6,32 @@ package map;
 
 import java.io.File;
 
+import render.GameScreen;
+
 public class Map {
+	
+	private int width, height;
 
 	public Map(File mapFile) {
 		
 	}
 	
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public boolean isOnScreen(int x, int y) {
+		return (x >= 0 && x <= this.getWidth()) && (y >= 0 && y <= this.getHeight());
+	}
+
 	public int getTerrain(int x, int y) {
 		//TODO terrain
+		if (this.isOnScreen(x, y))
+			return 0;
 		return 0;
 	}
 
