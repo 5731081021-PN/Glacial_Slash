e9c6c9ed32a536ba9da5a0844d3fcd1f7c82e3a2
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-12-09 20:48:34 +0700

    Use Enum to represent input keys rather than the key itself
    Using key binding method for input handling

--- src/render/GameScreen.java
+++ src/render/GameScreen.java
@@ -1,24 +1,46 @@
 package render;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JComponent;
+import javax.swing.KeyStroke;
 
 import entity.PlayerCharacter;
 import entity.PlayerStatus;
+import input.InputUtility;
 import map.Map;
 
 public class GameScreen extends JComponent {
 
 	private static final long serialVersionUID = 8861317653703713044L;
 	
-	private static GameScreen screen = new GameScreen();
+	private static GameScreen screen;
 	private Map currentMap;
 	private PlayerStatus playerStatus;
 	private PlayerCharacter playerCharacter;
 	
 	public static GameScreen getScreen() {
+		if (screen == null)
+			screen = new GameScreen();
 		return screen;
 	}
 	
-	// On KeyListener, make triggering synchronous? Then pressing asynchronous?
+	private GameScreen() {
+		this.setPreferredSize(new Dimension(1024, 576));
+		InputMap inputMap = this.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		ActionMap actionMap = this.getActionMap();
+		// Using key binding
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
 
 }
