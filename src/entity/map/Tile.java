e56da598ceec4b2dc689468f10f8b707c1d36754
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-12-09 23:01:19 +0700

    Move maps into entity

---
+++ src/entity/map/Tile.java
@@ -0,0 +1,47 @@
+package entity.map;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import render.Renderable;
+
+public enum Tile implements Renderable {
+	
+	AIR (true, null),
+	GROUND (false, );
+	
+	private boolean passable;
+	private Image tileSprite;
+	
+	private Tile(boolean passable, Image tileSprite) {
+		this.passable = passable;
+		this.tileSprite = tileSprite;
+	}
+	
+	public boolean isPassable() {
+		return passable;
+	}
+	
+	public Image getTileSprite() {
+		return tileSprite;
+	}
+
+	@Override
+	public void render(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isVisible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getZ() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
