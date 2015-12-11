fe90c2f3ac111dd30411b43e8f944a180ecccf54
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-12-11 16:46:41 +0700

    Fix resource conflict

--- src/res/Resource.java
+++ src/res/Resource.java
@@ -11,13 +11,7 @@ public class Resource {
 	public static BufferedImage floorTile;
 	public static BufferedImage mana[],maxMana[],slash;
 	
-<<<<<<< HEAD
-=======
-	
 	public static InputStream emptyMap, testMap, bigMap;
->>>>>>> refs/remotes/5731025921-CJ/master
-
-	public static File emptyMap, testMap, bigMap;
 	
 	private static ClassLoader loader = Resource.class.getClassLoader();
 
@@ -35,35 +29,17 @@ public class Resource {
 			floorTile = null;
 		}
 
-<<<<<<< HEAD
-		try {
-			emptyMap = new File(loader.getResource("res/map/emptyMap.map").toURI());
-		} catch (URISyntaxException e) {
-			emptyMap = null;
-		}
-		try {
-			testMap = new File(loader.getResource("res/map/testMap.map").toURI());
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			testMap = null;
-		}
-		try {
-			bigMap = new File(loader.getResource("res/map/bigMap.map").toURI());
-		} catch (URISyntaxException e) {
-			bigMap = null;
-		}
-
 		// Load mana,maxMana and slash
 		for (int i = 0; i <= 20; i++) {
 			try {
-				mana[i] = ImageIO.read(loader.getResource("res/mana/i.png"));
+				mana[i] = ImageIO.read(loader.getResource("res/mana/" + i + ".png"));
 			} catch (IOException e ) {
 				mana[i] = null;
 			}
 		}
 		for (int i = 0; i <= 20; i++) {
 			try {
-				maxMana[i] = ImageIO.read(loader.getResource("res/mana/mi.png"));
+				maxMana[i] = ImageIO.read(loader.getResource("res/mana/m" + i + ".png"));
 			} catch (IOException e ) {
 				maxMana[i] = null;
 			}
@@ -73,12 +49,10 @@ public class Resource {
 			} catch (IOException e) {
 				slash = null;
 			}
-=======
+
 		emptyMap = loader.getResourceAsStream("res/map/emptyMap.map");
 		testMap = loader.getResourceAsStream("res/map/testMap.map");
 		bigMap = loader.getResourceAsStream("res/map/bigMap.map");
-//			bigMap = new File();
->>>>>>> refs/remotes/5731025921-CJ/master
 
 	}
 
