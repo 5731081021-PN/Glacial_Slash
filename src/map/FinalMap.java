f6fea7c5a3444b4818e72988df6130b1d03dac71
Chaiyapat Julsri <adelaide.8000@gmail.com>
2015-12-15 01:15:48 +0700

    Implement The End rendering

--- src/map/FinalMap.java
+++ src/map/FinalMap.java
@@ -1,13 +1,37 @@
 package map;
 
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.io.InputStream;
 
 import res.Resource;
 
 public class FinalMap extends GameMap {
+	
+	private CheckPoint finalCheckPoint;
+	private boolean isWin;
 
 	public FinalMap() {
 		super(Resource.finalMap);
+		finalCheckPoint = checkPoints[checkPoints.length-1];
+		isWin = false;
+	}
+	
+	@Override
+	public void render(Graphics2D g) {
+		super.render(g);
+	}
+		
+	public void renderTheEndContent(Graphics2D g) {
+		if (isWin)
+			g.drawImage(Resource.theEndContent, null, 0, 0);
+	}
+
+	@Override
+	public void collideCheckPoints(Rectangle collisionBox) {
+		super.collideCheckPoints(collisionBox);
+		if (finalCheckPoint.getBoundaries().intersects(collisionBox))
+			isWin = true;
 	}
 
 }
