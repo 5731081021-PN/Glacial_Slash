905183ba20da56a4c59bb275d2c26cecd4bf29b3
5731081021-PN <fordpittawas@gmail.com>
2015-12-16 02:20:55 +0700

    fix  content

--- src/map/TutorialMap.java
+++ src/map/TutorialMap.java
@@ -19,10 +19,10 @@ public class TutorialMap extends GameMap {
 		GameScreen gameScreen = GameScreen.getScreen();
 		g.drawImage(Resource.leftButton, null, 140 - gameScreen.getCameraX(), 900 - gameScreen.getCameraY());
 		g.drawImage(Resource.rightButton, null, 220 - gameScreen.getCameraX(), 900 - gameScreen.getCameraY());
-		g.drawImage(Resource.walkContent, null, 200 - gameScreen.getCameraX(), 980 - gameScreen.getCameraY());
+		g.drawImage(Resource.walkContent, null, 210 - gameScreen.getCameraX(), 980 - gameScreen.getCameraY());
 		g.drawImage(Resource.spaceButton, null, 1540 - gameScreen.getCameraX(), 1000 - gameScreen.getCameraY());
 		g.drawImage(Resource.jumpContent, null, 1570 - gameScreen.getCameraX(), 1080 - gameScreen.getCameraY());
-		g.drawImage(Resource.manaSourceContent, null, 2870 - gameScreen.getCameraX(), 960 - gameScreen.getCameraY());
+		g.drawImage(Resource.manaSourceContent, null, 2900 - gameScreen.getCameraX(), 960 - gameScreen.getCameraY());
 		g.drawImage(Resource.downButton, null, 3640 - gameScreen.getCameraX(), 800 - gameScreen.getCameraY());
 		g.drawImage(Resource.plusButton, null, 3740 - gameScreen.getCameraX(), 840 - gameScreen.getCameraY());
 		g.drawImage(Resource.sButton, null, 3720 - gameScreen.getCameraX(), 800 - gameScreen.getCameraY());
@@ -32,9 +32,9 @@ public class TutorialMap extends GameMap {
 		g.drawImage(Resource.upButton, null, 8240 - gameScreen.getCameraX(), 800 - gameScreen.getCameraY());
 		g.drawImage(Resource.plusButton, null, 8340 - gameScreen.getCameraX(), 840 - gameScreen.getCameraY());
 		g.drawImage(Resource.fButton, null, 8320 - gameScreen.getCameraX(), 800 - gameScreen.getCameraY());
-		g.drawImage(Resource.skyUppercutContent, null, 8250 - gameScreen.getCameraX(), 780 - gameScreen.getCameraY());
+		g.drawImage(Resource.skyUppercutContent, null, 8270 - gameScreen.getCameraX(), 780 - gameScreen.getCameraY());
 		g.drawImage(Resource.eButton, null, 9500 - gameScreen.getCameraX(), 750 - gameScreen.getCameraY());
-		g.drawImage(Resource.concentrationContent, null, 9470 - gameScreen.getCameraX(), 730 - gameScreen.getCameraY());
+		g.drawImage(Resource.concentrationContent, null, 9480 - gameScreen.getCameraX(), 730 - gameScreen.getCameraY());
 		g.drawImage(Resource.rightButton, null, 9980 - gameScreen.getCameraX(), 400 - gameScreen.getCameraY());
 		g.drawImage(Resource.plusButton, null, 10080 - gameScreen.getCameraX(), 440 - gameScreen.getCameraY());
 		g.drawImage(Resource.dButton, null, 10060 - gameScreen.getCameraX(), 400 - gameScreen.getCameraY());
